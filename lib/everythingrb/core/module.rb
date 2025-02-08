0fd0305034b2faf8458ebd15f6674d8c05a48549
Bryan <bryan@itsthedevman.com>
2025-02-07 23:31:13 -0500

    Added Struct/Data support to attr_predicate

--- lib/everythingrb/core/module.rb
+++ lib/everythingrb/core/module.rb
@@ -2,24 +2,49 @@
 
 class Module
   #
-  # Creates predicate (boolean) methods for instance variables
-  # Similar to attr_reader, attr_writer, etc.
+  # Creates predicate (boolean) methods that return true/false
+  # Similar to attr_reader, attr_writer, etc. Designed to work with
+  # regular classes, Struct, and Data objects.
   #
-  # @param *attributes [Array<Symbol, String>] Instance variable names
+  # @param *attributes [Array<Symbol, String>] Attribute names
+  #
   # @return [nil]
   #
-  # @example
+  # @raise [ArgumentError] If a predicate method of the same name already exists
+  #
+  # @example With a regular class
   #   class User
-  #     attr_predicate :admin, :active
+  #     attr_predicate :admin
+  #     attr_accessor :admin
   #   end
   #
-  #   user.admin? # => true/false based on @admin
+  #   user = User.new
+  #   user.admin? # => false
+  #   user.admin = true
+  #   user.admin? # => true
+  #
+  # @example With Struct/Data
+  #   Person = Struct.new(:active)
+  #   Person.attr_predicate(:active)
+  #
+  #   person = Person.new(active: true)
+  #   person.active? # => true
   #
   def attr_predicate(*attributes)
     attributes.each do |attribute|
+      if method_defined?(:"#{attribute}?")
+        raise ArgumentError, "Cannot create predicate method on #{self.class} - #{attribute}? is already defined. Please choose a different name or remove the existing method."
+      end
+
       module_eval <<-STR, __FILE__, __LINE__ + 1
         def #{attribute}?
-          !!instance_variable_get("@#{attribute}")
+          if instance_variable_defined?(:@#{attribute})
+            !!@#{attribute}
+          elsif respond_to?(:#{attribute})
+            !!self.#{attribute}
+          else
+            false
+          end
         end
       STR
     end
--- test/core/module/test_attr_predicate.rb
+++ test/core/module/test_attr_predicate.rb
@@ -3,7 +3,6 @@
 require "test_helper"
 
 class AttrPredicateMock
-  attr_reader :some_variable
   attr_predicate :some_variable
 
   def initialize(value)
@@ -15,7 +14,38 @@ class TestModuleAttrPredicate < Minitest::Test
   def test_defines_the_predicate
     object = AttrPredicateMock.new(42)
 
-    assert_equal(42, object.some_variable)
+    assert_equal(42, object.instance_variable_get(:@some_variable))
     assert(object.some_variable?)
+  end
+
+  def test_it_works_with_structs
+    klass = Struct.new(:foo)
+    klass.attr_predicate(:foo)
+
+    object = klass.new(foo: 42)
+
+    assert_equal(42, object.foo)
+    assert(object.foo?)
+  end
+
+  def test_it_works_with_false
+    klass = Data.define(:foo)
+    klass.attr_predicate(:foo)
+
+    object = klass.new(foo: false)
+
+    assert_equal(false, object.foo)
+    refute(object.foo?)
+  end
+
+  def test_it_raises_on_method_defined
+    klass = Data.define(:foo) do
+      def foo?
+        puts "foo"
+      end
+    end
+
+    binding.pry
+    assert_raises(ArgumentError) { klass.attr_predicate(:foo) }
   end
 end
